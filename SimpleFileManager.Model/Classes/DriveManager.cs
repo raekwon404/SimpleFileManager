81b5fbe7ea3890fe023ea7eabce602b94d02a640
Dmytro P <atosdo@live.com>
2015-08-02 22:35:59 +0300

    *

---
+++ SimpleFileManager.Model/Classes/DriveManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFileManager.Model.Classes
+{
+    public static class DriveManager
+    {
+        public static IEnumerable<DriveInfo> GetAllDrives() => DriveInfo.GetDrives();
+    }
+}
