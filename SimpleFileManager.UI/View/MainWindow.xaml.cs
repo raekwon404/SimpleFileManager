e8afc6055bd8dfb9bec71ce206a5bef625a1d2b9
Dmytro P <atosdo@live.com>
2015-08-04 22:09:41 +0300

    Add drives button load

--- SimpleFileManager.UI/View/MainWindow.xaml.cs
+++ SimpleFileManager.UI/View/MainWindow.xaml.cs
@@ -1,19 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using SimpleFileManager.UI.ViewModel;
 
-namespace SimpleFileManager.UI
+namespace SimpleFileManager.UI.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,6 +12,11 @@ namespace SimpleFileManager.UI
         public MainWindow()
         {
             InitializeComponent();
+
+            MainWindowViewModel windowViewModel = new MainWindowViewModel();
+            DataContext = windowViewModel;            
+
+            driveButtons.ItemsSource = windowViewModel.Drives;
         }
     }
 }
--- SimpleFileManager.UI/ViewModel/MainWindowViewModel.cs
+++ SimpleFileManager.UI/ViewModel/MainWindowViewModel.cs
@@ -1,13 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using SimpleFileManager.Model.Classes;
 
 namespace SimpleFileManager.UI.ViewModel
 {
-    class MainWindowViewModel
+    class MainWindowViewModel : INotifyPropertyChanged
     {
-        
+        #region Properties
+
+        public ObservableCollection<DriveInfo> Drives { get; private set; }
+
+        #endregion
+
+        #region Ctors
+
+        public MainWindowViewModel()
+        {
+            Drives = new ObservableCollection<DriveInfo>(DriveManager.GetAllDrives());
+        }
+
+        #endregion
+
+        #region Commands
+
+        private ICommand _clickDriveCommand;
+
+        #endregion
+
+        #region INotifyPropertyChanged Implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
     }
 }
